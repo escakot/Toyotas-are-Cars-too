2981f33e653693436bb6563aecede4630d4dd7c2
escakot <ec@neorennie.com>
2017-06-03 03:41:42 -0400

    Initial Commit

---
+++ Cars/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cars
+//
+//  Created by Errol Cheong on 2017-06-03.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
